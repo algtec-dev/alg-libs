9d1baba0dfe54d96f72bb48ee7890f1af3ef1cce
Mateus D'Almeida <mateusdalmeida0@gmail.com>
2022-06-10 13:16:21 -0300

    msg para informar a cor

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import AlgCrud from './src/alg-crud/alg-crud.vue'
 import AlgLogin from './src/alg-login/alg-login.vue'
 
 function install(Vue, options = {}) {
+    if (!options.color) console.warn('ALGLIBS: Informe um color');
+
     Vue.prototype.$primaryColor = options.color ?? 'grey'
 
     Vue.component('AlgCrud', AlgCrud)
