9119f26d27ceed6c371aa655fa63c42917161967
Mateus D Almeida <mateusdalmeida0@gmail.com>
2024-05-16 19:52:43 -0300

    novas validacoes

--- src/utils/rules.js
+++ src/utils/rules.js
@@ -1,3 +1,7 @@
+import utils from "./utils";
+
+// validações genericas
+
 function sizeRule(size, text) {
   return (value) => {
     if (!value) return true;
@@ -45,6 +49,69 @@ function requiredMultipleSelect(text) {
   return (value) => value.length > 0 || (text ?? "Campo Obrigatório");
 }
 
+// validações especificas
+
+function cpfRule(text) {
+  return (value) => {
+    if (!value) return true;
+    return utils.validaCpf(value) || (text ?? "CPF Inválido");
+  };
+}
+
+function emailRule(text) {
+  const regex =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return (value) => {
+    if (!value) return true;
+    return regex.test(value) || (text ?? "Email incorreto");
+  };
+}
+
+function brDateRule(text) {
+  return (value) => {
+    if (!value) return true;
+    return utils.isValidDate(value) || (text ?? "Data incorreta");
+  };
+}
+
+// Função para validar uma data mínima
+function minBrDateRule(minDate, text) {
+  if (!utils.isValidDate(minDate)) {
+    throw new Error("Data mínima fornecida não está no formato correto.");
+  }
+  return function (value) {
+    if (!value) return true;
+    if (!utils.isValidDate(value)) return text ?? "Data incorreta";
+    var [minDay, minMonth, minYear] = minDate.split("/").map(Number);
+    var [day, month, year] = value.split("/").map(Number);
+    var date = new Date(year, month - 1, day);
+    var minDateTime = new Date(minYear, minMonth - 1, minDay);
+    return (
+      date >= minDateTime ||
+      (text ?? `Data deve ser maior ou igual a ${minDate}`)
+    );
+  };
+}
+
+// Função para validar uma data máxima
+function maxBrDateRule(maxDate, text) {
+  if (!utils.isValidDate(maxDate)) {
+    throw new Error("Data máxima fornecida não está no formato correto.");
+  }
+  return function (value) {
+    if (!value) return true;
+    if (!utils.isValidDate(value)) return text ?? "Data incorreta";
+    var [maxDay, maxMonth, maxYear] = maxDate.split("/").map(Number);
+    var [day, month, year] = value.split("/").map(Number);
+    var date = new Date(year, month - 1, day);
+    var maxDateTime = new Date(maxYear, maxMonth - 1, maxDay);
+    return (
+      date <= maxDateTime ||
+      (text ?? `Data deve ser menor ou igual a ${maxDate}`)
+    );
+  };
+}
+
 export default {
   regexRule,
   sizeRule,
@@ -52,4 +119,9 @@ export default {
   maxSizeRule,
   required,
   requiredMultipleSelect,
+  cpfRule,
+  emailRule,
+  brDateRule,
+  minBrDateRule,
+  maxBrDateRule,
 };
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -18,7 +18,43 @@ function getOnlyDate(x) {
   return value;
 }
 
+function validaCpf(c) {
+  if (!c) return false;
+  const notDig = (i) => ![".", "-", " "].includes(i);
+  const prepare = (cpf) => cpf.trim().split("").filter(notDig).map(Number);
+  const is11Len = (cpf) => cpf.length === 11;
+  const notAllEquals = (cpf) => !cpf.every((i) => cpf[0] === i);
+  const onlyNum = (cpf) => cpf.every((i) => !isNaN(i));
+
+  const calcDig = (limit) => (a, i, idx) => a + i * (limit + 1 - idx);
+  const somaDig = (cpf, limit) => cpf.slice(0, limit).reduce(calcDig(limit), 0);
+  const resto11 = (somaDig) => 11 - (somaDig % 11);
+  const zero1011 = (resto11) => ([10, 11].includes(resto11) ? 0 : resto11);
+
+  const getDV = (cpf, limit) => zero1011(resto11(somaDig(cpf, limit)));
+  const verDig = (pos) => (cpf) => getDV(cpf, pos) === cpf[pos];
+
+  const checks = [is11Len, notAllEquals, onlyNum, verDig(9), verDig(10)];
+  const checkAll = (cpf) => checks.map((f) => f(cpf)).every((r) => !!r);
+
+  return checkAll(prepare(c));
+}
+
+function isValidDate(dateString) {
+  var regex = /^(\d{2})\/(\d{2})\/(\d{4})$/;
+  if (!regex.test(dateString)) return false;
+  var [day, month, year] = dateString.split("/").map(Number);
+  var date = new Date(year, month - 1, day);
+  return (
+    date.getDate() === day &&
+    date.getMonth() === month - 1 &&
+    date.getFullYear() === year
+  );
+}
+
 export default {
   getHumanDate,
   getOnlyDate,
+  validaCpf,
+  isValidDate,
 };
